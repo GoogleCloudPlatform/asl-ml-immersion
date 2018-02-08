5a09af7302eedf0f9d206c979fcfe64f2baa1a67
Ryan Gillard <ryangillard@google.com>
2018-02-08 10:38:07 -0800

    Fixed hyperparameter typo in 2.4

---
+++ 2.4_endtoend_lab/babyweight/trainer/model.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import shutil
+import numpy as np
+import tensorflow as tf
+
+# Set logging to be level of INFO
+tf.logging.set_verbosity(tf.logging.INFO)
+
+BUCKET = None  # set from task.py
+PATTERN = 'of' # gets all files
+
+# Determine CSV, label, and key columns
+CSV_COLUMNS = 'weight_pounds,is_male,mother_age,mother_race,plurality,gestation_weeks,mother_married,cigarette_use,alcohol_use,key'.split(',')
+LABEL_COLUMN = 'weight_pounds'
+KEY_COLUMN = 'key'
+
+# Set default values for each CSV column
+DEFAULTS = [[0.0], ['null'], [0.0], ['null'], [0.0], [0.0], ['null'], ['null'], ['null'], ['nokey']]
+
+# Define some hyperparameters
+TRAIN_STEPS = 10000
+BATCH_SIZE = 512
+EMBED_SIZE = 3
+HIDDEN_UNITS = [64, 16, 4]
+
+# Create an input function reading a file using the Dataset API
+# Then provide the results to the Estimator API
+def read_dataset(prefix, mode, batch_size = 512):
+    def _input_fn():
+        def decode_csv(value_column):
+            columns = tf.decode_csv(value_column, record_defaults = DEFAULTS)
+            features = dict(zip(CSV_COLUMNS, columns))
+            label = features.pop(LABEL_COLUMN)
+            return features, label
+        
+        # Use prefix to create file path
+        file_path = 'gs://{}/babyweight/preproc/{}*{}*'.format(BUCKET, prefix, PATTERN)
+
+        # Create list of files that match pattern
+        file_list = tf.gfile.Glob(file_path)
+
+        # Create dataset from file list
+        dataset = (tf.data.TextLineDataset(file_list)  # Read text file
+                    .map(decode_csv))  # Transform each elem by applying decode_csv fn
+      
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            num_epochs = None # indefinitely
+            dataset = dataset.shuffle(buffer_size = 10 * batch_size)
+        else:
+            num_epochs = 1 # end-of-input after this
+ 
+        dataset = dataset.repeat(num_epochs).batch(batch_size)
+        return dataset.make_one_shot_iterator().get_next()
+    return _input_fn
+
+# Define feature columns
+def get_wide_deep():
+    # TODO: what are your feature columns?
+    
+    return wide, deep
+
+# Create serving input function to be able to serve predictions later using provided inputs
+def serving_input_fn():
+    feature_placeholders = {
+      'is_male': tf.placeholder(tf.string, [None]),
+      'mother_age': tf.placeholder(tf.float32, [None]),
+      'mother_race': tf.placeholder(tf.string, [None]),
+      'plurality': tf.placeholder(tf.float32, [None]),
+      'gestation_weeks': tf.placeholder(tf.float32, [None]),
+      'mother_married': tf.placeholder(tf.string, [None]),
+      'cigarette_use': tf.placeholder(tf.string, [None]),
+      'alcohol_use': tf.placeholder(tf.string, [None])
+    }
+    features = {
+      key: tf.expand_dims(tensor, -1)
+      for key, tensor in feature_placeholders.items()
+    }
+    return tf.estimator.export.ServingInputReceiver(features, feature_placeholders)
+
+# Create estimator to train and evaluate
+def train_and_evaluate(output_dir):
+    wide, deep = get_wide_deep()
+    estimator = tf.estimator.DNNLinearCombinedRegressor(
+        model_dir = output_dir,
+        linear_feature_columns = wide,
+        dnn_feature_columns = deep,
+        dnn_hidden_units = HIDDEN_UNITS)
+    train_spec = tf.estimator.TrainSpec(
+        input_fn = read_dataset('train.csv', mode = tf.estimator.ModeKeys.TRAIN, BATCH_SIZE),
+        max_steps = TRAIN_STEPS)
+    exporter = tf.estimator.LatestExporter('exporter', serving_input_fn)
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn = read_dataset('eval.csv', mode = tf.estimator.ModeKeys.EVAL, BATCH_SIZE),
+        steps = None,
+        start_delay_secs = 60, # start evaluating after N seconds
+        throttle_secs = 300,  # evaluate every N seconds
+        exporters = exporter)
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
---
+++ 2.4_endtoend_soln/babyweight/trainer/task.py
@@ -0,0 +1,101 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example implementation of code to run on the Cloud ML service.
+"""
+
+import argparse
+import json
+import os
+
+import model
+
+import tensorflow as tf
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--bucket',
+        help = 'GCS path to data. We assume that data is in gs://BUCKET/babyweight/preproc/',
+        required = True
+    )
+    parser.add_argument(
+        '--output_dir',
+        help = 'GCS location to write checkpoints and export models',
+        required = True
+    )
+    parser.add_argument(
+        '--train_examples',
+        help = 'Number of examples (in thousands) to run the training job over. If this is more than actual # of examples available, it cycles through them. So specifying 1000 here when you have only 100k examples makes this 10 epochs.',
+        type = int,
+        default = 5000
+    )
+    parser.add_argument(
+        '--batch_size',
+        help = 'Number of examples to compute gradient over.',
+        type = int,
+        default = 512
+    )
+    parser.add_argument(
+        '--embed_size',
+        help = 'Embedding size of a cross of n key real-valued parameters',
+        type = int,
+        default = 3
+    )
+    parser.add_argument(
+        '--hidden_units',
+        help='Hidden layer sizes to use for DNN feature columns -- provide space-separated layers',
+        nargs='+',
+        type=int,
+        default=[128, 32, 4]
+    )
+    parser.add_argument(
+        '--pattern',
+        help = 'Specify a pattern that has to be in input files. For example 00001-of will process only one shard',
+        default = 'of'
+    )
+    parser.add_argument(
+        '--job-dir',
+        help = 'this model ignores this field, but it is required by gcloud',
+        default = 'junk'
+    )
+
+    args = parser.parse_args()
+    arguments = args.__dict__
+
+    # Unused args provided by service
+    arguments.pop('job_dir', None)
+    arguments.pop('job-dir', None)
+
+    output_dir = arguments.pop('output_dir')
+    model.BUCKET = arguments.pop('bucket')
+    model.BATCH_SIZE = arguments.pop('batch_size')
+    model.TRAIN_STEPS = (arguments.pop('train_examples') * 1000) / model.BATCH_SIZE
+    print ("Will train for {} steps using batch_size={}".format(model.TRAIN_STEPS, model.BATCH_SIZE))
+    model.PATTERN = arguments.pop('pattern')
+    model.EMEBED_SIZE = arguments.pop('embed_size')
+    model.HIDDEN_UNITS = arguments.pop('hidden_units')
+    print ("Will use DNN size of {}".format(model.HIDDEN_UNITS))
+
+    # Append trial_id to path if we are doing hptuning
+    # This code can be removed if you are not using hyperparameter tuning
+    output_dir = os.path.join(
+        output_dir,
+        json.loads(
+            os.environ.get('TF_CONFIG', '{}')
+        ).get('task', {}).get('trial', '')
+    )
+
+    # Run the training job
+    model.train_and_evaluate(output_dir)

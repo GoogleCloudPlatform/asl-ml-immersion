e6a12a6579ff6db5b17cb6f5066ce3b977eee4f2
BenoitDherin <dherin@google.com>
2021-08-26 15:38:11 +0000

    remove obsolete mlops folder

--- notebooks/mlops/docker_and_kubernetes/solutions/src/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var handleRequest = function(request, response) {
-  response.writeHead(200);
-  response.end("Hello World!\n");
-}
-var www = http.createServer(handleRequest);
-www.listen(8000);

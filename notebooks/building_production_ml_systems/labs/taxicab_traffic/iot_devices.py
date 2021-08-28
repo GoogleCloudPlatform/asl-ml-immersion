a64d16010f0c2d50fd6fb62f67dae79c331a35d5
Daniel Sparing <dsparing@google.com>
2021-08-28 03:05:45 +0000

    copy from solutions to labs

--- notebooks/building_production_ml_systems/labs/taxicab_traffic/iot_devices.py
+++ notebooks/building_production_ml_systems/labs/taxicab_traffic/iot_devices.py
@@ -1,20 +1,24 @@
-import time
-import gzip
-import logging
+"""
+Send sensor data to Cloud Pub/Sub in small groups, simulating real-time
+behavior
+"""
+
 import argparse
-import datetime
+import logging
 import random
+import time
+
+from google import api_core
 from google.cloud import pubsub
 
-"""
-Send sensor data to Cloud Pub/Sub in small groups, simulating real-time behavior
-"""
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 DEFAULT_TOPIC = "taxi_rides"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--project", help="Google Cloud Project ID", required=True)
+    parser.add_argument(
+        "--project", help="Google Cloud Project ID", required=True
+    )
     parser.add_argument(
         "--topic",
         help="Pub/Sub Topic, will be created if doesn't exist",
@@ -23,19 +27,21 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # create Pub/Sub notification topic
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+    logging.basicConfig(
+        format="%(levelname)s: %(message)s", level=logging.INFO
+    )
     publisher = pubsub.PublisherClient()
     topic_name = publisher.topic_path(args.project, args.topic)
     try:
         publisher.get_topic(topic_name)
-        logging.info("Reusing pub/sub topic {}".format(args.topic))
-    except:
+        logging.info("Reusing pub/sub topic %s", args.topic)
+    except api_core.exceptions.NotFound:
         publisher.create_topic(topic_name)
-        logging.info("Creating pub/sub topic {}".format(args.topic))
+        logging.info("Creating pub/sub topic %s", args.topic)
 
     while True:
         num_trips = random.randint(10, 60)
         for i in range(num_trips):
             publisher.publish(topic_name, b"taxi_ride")
-        logging.info("Publishing: {}".format(time.ctime()))
+        logging.info("Publishing: %s", time.ctime())
         time.sleep(5)

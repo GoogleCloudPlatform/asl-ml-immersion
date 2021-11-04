5b314ede8af3505d6143a876c733c359cb374e65
Daniel Sparing <dsparing@google.com>
2021-11-04 21:59:58 +0000

    attempt to loop over list of notebooks

--- executor/notebook_executor.sh
+++ executor/notebook_executor.sh
@@ -1,30 +1,47 @@
 TF_VERSION=2-6
 LOCATION=us-central1
 MASTER_TYPE=n1-standard-4
-INPUT_NOTEBOOK_FILE=gs://dsparing-sandbox/Untitled.ipynb
-#CONTAINER_IMAGE_URI=gcr.io/deeplearning-platform-release/tf2-cpu.$TF_VERSION
-CONTAINER_IMAGE_URI=gcr.io/deeplearning-platform-release/base-cpu
-OUTPUT_NOTEBOOK_FOLDER=gs://dsparing-sandbox/broaf
-EXECUTION_ID=execution_$(date +%Y-%m-%dT%H%M%S)
+TIMESTAMP=$(date +%Y-%m-%dT%H%M%S)
+STAGING_BUCKET=asl_cloudbuild_$TIMESTAMP
+CONTAINER_IMAGE_URI=gcr.io/deeplearning-platform-release/tf2-cpu.$TF_VERSION
+EXECUTION_ID=execution_$TIMESTAMP
+GIT_ORG=https://github.com/GoogleCloudPlatform
+GIT_REPO=asl-ml-immersion
+NOTEBOOK_TEST_ALLOWLIST="$GIT_REPO/notebooks/introduction_to_tensorflow/solutions/1_core_tensorflow.ipynb
+$GIT_REPO/notebooks/introduction_to_tensorflow/solutions/4_keras_functional_api.ipynb
+"
 
-echo > request.json "{
-  \"executionTemplate\": {
-      \"masterType\": \"$MASTER_TYPE\",
-      \"inputNotebookFile\": \"$INPUT_NOTEBOOK_FILE\",
-      \"containerImageUri\": \"$CONTAINER_IMAGE_URI\",
-      \"outputNotebookFolder\": \"$OUTPUT_NOTEBOOK_FOLDER\",
-  },
-}"
+git clone GIT_REPO
 
-PROJECT_ID=$(gcloud config get-value project)
-PROJECT_NUMBER=$(gcloud projects list --filter="name=$PROJECT_ID" --format="value(PROJECT_NUMBER)")
-LOCATION=$LOCATION
-PARENT=projects/$PROJECT_NUMBER/locations/$LOCATION
+gsutil mk -l $LOCATION STAGING_BUCKET
 
-echo $PARENT
+for NOTEBOOK in NOTEBOOK_TEST_ALLOWLIST ; do
 
-curl -X POST \
--H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
--H "Content-Type: application/json; charset=utf-8" \
--d @request.json \
-"https://notebooks.googleapis.com/v1/$PARENT/executions?executionId=$EXECUTION_ID"
+    INPUT_NOTEBOOK_FILE=gs://STAGING_BUCKET/NOTEBOOK
+    OUTPUT_NOTEBOOK_FOLDER=gs://STAGING_BUCKET/NOTEBOOK/output
+    
+    gsutil cp NOTEBOOK INPUT_NOTEBOOK_FILE
+
+    echo > request.json "{
+      \"executionTemplate\": {
+          \"masterType\": \"$MASTER_TYPE\",
+          \"inputNotebookFile\": \"$INPUT_NOTEBOOK_FILE\",
+          \"containerImageUri\": \"$CONTAINER_IMAGE_URI\",
+          \"outputNotebookFolder\": \"$OUTPUT_NOTEBOOK_FOLDER\",
+      },
+    }"
+
+    PROJECT_ID=$(gcloud config get-value project)
+    PROJECT_NUMBER=$(gcloud projects list --filter="name=$PROJECT_ID" --format="value(PROJECT_NUMBER)")
+    LOCATION=$LOCATION
+    PARENT=projects/$PROJECT_NUMBER/locations/$LOCATION
+
+    echo $PARENT
+
+    curl -X POST \
+    -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d @request.json \
+    "https://notebooks.googleapis.com/v1/$PARENT/executions?executionId=$EXECUTION_ID"
+
+done

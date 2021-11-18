1fd6d21e4274ed236e9ec3d50effea500297e7ff
Daniel Sparing <dsparing@google.com>
2021-11-18 23:50:56 +0000

    black run

--- notebooks/feature_engineering/labs/tft_trainer/model.py
+++ notebooks/feature_engineering/labs/tft_trainer/model.py
@@ -1,7 +1,8 @@
-import numpy as np
-import os
-import tensorflow as tf
 import datetime
+import os
+
+import numpy as np
+import tensorflow as tf
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
 
@@ -29,20 +30,20 @@ def create_dataset(file_pattern, batch_size, mode):
         """
 
         feature_map = {
-            'dayofweek': tf.io.FixedLenFeature([], tf.int64),
-            'dropofflat': tf.io.FixedLenFeature([], tf.float32),
-            'dropofflon': tf.io.FixedLenFeature([], tf.float32),
-            'fare_amount': tf.io.FixedLenFeature([], tf.float32),
-            'hourofday': tf.io.FixedLenFeature([], tf.int64),
-            'passengers': tf.io.FixedLenFeature([], tf.float32),
-            'pickuplat': tf.io.FixedLenFeature([], tf.float32),
-            'pickuplon': tf.io.FixedLenFeature([], tf.float32)
+            "dayofweek": tf.io.FixedLenFeature([], tf.int64),
+            "dropofflat": tf.io.FixedLenFeature([], tf.float32),
+            "dropofflon": tf.io.FixedLenFeature([], tf.float32),
+            "fare_amount": tf.io.FixedLenFeature([], tf.float32),
+            "hourofday": tf.io.FixedLenFeature([], tf.int64),
+            "passengers": tf.io.FixedLenFeature([], tf.float32),
+            "pickuplat": tf.io.FixedLenFeature([], tf.float32),
+            "pickuplon": tf.io.FixedLenFeature([], tf.float32),
         }
 
         # Parse the serialized data into a dictionary.
         parsed_example = tf.io.parse_single_example(
-            serialized=serialized_example,
-            features=feature_map)
+            serialized=serialized_example, features=feature_map
+        )
 
         features = add_engineered(parsed_example)
         label = features.pop("fare_amount")
@@ -52,16 +53,14 @@ def create_dataset(file_pattern, batch_size, mode):
     # Create a TensorFlow Dataset-object which has functionality
     # for reading and shuffling data from TFRecords files.
     files = tf.io.gfile.glob(file_pattern)
-    dataset = tf.data.TFRecordDataset(
-        filenames=files, compression_type="GZIP")
+    dataset = tf.data.TFRecordDataset(filenames=files, compression_type="GZIP")
 
     # Parse the serialized data in the TFRecords files..
     dataset = dataset.map(_parse)
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         # Repeat the dataset the given number of times.
-        dataset = dataset.repeat().shuffle(
-            buffer_size=10*batch_size)
+        dataset = dataset.repeat().shuffle(buffer_size=10 * batch_size)
 
     # Get a batch of data with the given size.
     dataset = dataset.batch(batch_size)
@@ -82,7 +81,8 @@ def add_engineered(features):
     features["londiff"] = features["dropofflon"] - features["pickuplon"]
     features["latdiff"] = features["dropofflat"] - features["pickuplat"]
     features["euclidean"] = tf.math.sqrt(
-        features["londiff"]**2 + features["latdiff"]**2)
+        features["londiff"] ** 2 + features["latdiff"] ** 2
+    )
     return features
 
 
@@ -95,25 +95,33 @@ def serving_input_fn():
     """
     inputs = {
         "dayofweek": tf.compat.v1.placeholder(
-            dtype=tf.dtypes.int64, shape=[None], name="dayofweek"),
+            dtype=tf.dtypes.int64, shape=[None], name="dayofweek"
+        ),
         "hourofday": tf.compat.v1.placeholder(
-            dtype=tf.dtypes.int64, shape=[None], name="hourofday"),
+            dtype=tf.dtypes.int64, shape=[None], name="hourofday"
+        ),
         "pickuplon": tf.compat.v1.placeholder(
-            dtype=tf.dtypes.float32, shape=[None], name="pickuplon"),
+            dtype=tf.dtypes.float32, shape=[None], name="pickuplon"
+        ),
         "pickuplat": tf.compat.v1.placeholder(
-            dtype=tf.dtypes.float32, shape=[None], name="pickuplat"),
+            dtype=tf.dtypes.float32, shape=[None], name="pickuplat"
+        ),
         "dropofflon": tf.compat.v1.placeholder(
-            dtype=tf.dtypes.float32, shape=[None], name="dropofflon"),
+            dtype=tf.dtypes.float32, shape=[None], name="dropofflon"
+        ),
         "dropofflat": tf.compat.v1.placeholder(
-            dtype=tf.dtypes.float32, shape=[None], name="dropofflat"),
+            dtype=tf.dtypes.float32, shape=[None], name="dropofflat"
+        ),
         "passengers": tf.compat.v1.placeholder(
-            dtype=tf.dtypes.float32, shape=[None], name="passengers")
+            dtype=tf.dtypes.float32, shape=[None], name="passengers"
+        ),
     }
 
     features = add_engineered(inputs)
 
     return tf.estimator.export.ServingInputReceiver(
-        features=features, receiver_tensors=inputs)
+        features=features, receiver_tensors=inputs
+    )
 
 
 def train_and_evaluate(args):
@@ -123,43 +131,45 @@ def train_and_evaluate(args):
         args: dict, dictionary of command line arguments from task.py.
     """
     feat_cols = [
-        tf.feature_column.numeric_column('dayofweek'),
-        tf.feature_column.numeric_column('hourofday'),
-        tf.feature_column.numeric_column('pickuplat'),
-        tf.feature_column.numeric_column('pickuplon'),
-        tf.feature_column.numeric_column('dropofflat'),
-        tf.feature_column.numeric_column('dropofflon'),
-        tf.feature_column.numeric_column('passengers'),
-        tf.feature_column.numeric_column('euclidean'),
-        tf.feature_column.numeric_column('latdiff'),
-        tf.feature_column.numeric_column('londiff')
+        tf.feature_column.numeric_column("dayofweek"),
+        tf.feature_column.numeric_column("hourofday"),
+        tf.feature_column.numeric_column("pickuplat"),
+        tf.feature_column.numeric_column("pickuplon"),
+        tf.feature_column.numeric_column("dropofflat"),
+        tf.feature_column.numeric_column("dropofflon"),
+        tf.feature_column.numeric_column("passengers"),
+        tf.feature_column.numeric_column("euclidean"),
+        tf.feature_column.numeric_column("latdiff"),
+        tf.feature_column.numeric_column("londiff"),
     ]
 
     estimator = tf.estimator.DNNRegressor(
         feature_columns=feat_cols,
-        hidden_units=args['hidden_units'].split(' '),
-        model_dir=args['output_dir']
+        hidden_units=args["hidden_units"].split(" "),
+        model_dir=args["output_dir"],
     )
 
     train_spec = tf.estimator.TrainSpec(
         input_fn=lambda: create_dataset(
-            file_pattern=args['train_data_path'],
-            batch_size=args['train_batch_size'],
-            mode=tf.estimator.ModeKeys.TRAIN),
-        max_steps=300
+            file_pattern=args["train_data_path"],
+            batch_size=args["train_batch_size"],
+            mode=tf.estimator.ModeKeys.TRAIN,
+        ),
+        max_steps=300,
     )
 
     exporter = exporter = tf.estimator.LatestExporter(
-        name="exporter",
-        serving_input_receiver_fn=serving_input_fn)
+        name="exporter", serving_input_receiver_fn=serving_input_fn
+    )
 
     eval_spec = tf.estimator.EvalSpec(
         input_fn=lambda: create_dataset(
-            file_pattern=args['eval_data_path'],
-            batch_size=args['eval_batch_size'],
-            mode=tf.estimator.ModeKeys.EVAL),
+            file_pattern=args["eval_data_path"],
+            batch_size=args["eval_batch_size"],
+            mode=tf.estimator.ModeKeys.EVAL,
+        ),
         exporters=exporter,
-        steps=50
+        steps=50,
     )
 
     tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)

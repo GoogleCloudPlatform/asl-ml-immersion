b5fb684478c9dcf899d8f86db04579dd306d7509
Daniel Sparing <dsparing@google.com>
2021-11-24 21:18:56 +0000

    linting

--- notebooks/kubeflow_pipelines/cicd/solutions/pipeline_vertex/training_lightweight_component.py
+++ notebooks/kubeflow_pipelines/cicd/solutions/pipeline_vertex/training_lightweight_component.py
@@ -13,24 +13,24 @@
 """Lightweight component training function."""
 
 
-
 def train_and_deploy(
-        project: str,
-        location: str,
-        container_uri: str,
-        serving_container_uri: str,
-        training_file_path: str,
-        validation_file_path: str,
-        staging_bucket: str,
-        alpha: float,
-        max_iter: int,
+    project: str,
+    location: str,
+    container_uri: str,
+    serving_container_uri: str,
+    training_file_path: str,
+    validation_file_path: str,
+    staging_bucket: str,
+    alpha: float,
+    max_iter: int,
 ):
     from google.cloud import aiplatform
 
-    aiplatform.init(project=project, location=location,
-                    staging_bucket=staging_bucket)
+    aiplatform.init(
+        project=project, location=location, staging_bucket=staging_bucket
+    )
     job = aiplatform.CustomContainerTrainingJob(
-        display_name='covertype_kfp_training',
+        display_name="covertype_kfp_training",
         container_uri=container_uri,
         command=[
             "python",
@@ -39,13 +39,13 @@ def train_and_deploy(
             f"--validation_dataset_path={validation_file_path}",
             f"--alpha={alpha}",
             f"--max_iter={max_iter}",
-            "--nohptune"
+            "--nohptune",
         ],
         staging_bucket=staging_bucket,
         model_serving_container_image_uri=serving_container_uri,
     )
-    model = job.run(replica_count=1, model_display_name='covertype_kfp_model')
+    model = job.run(replica_count=1, model_display_name="covertype_kfp_model")
     endpoint = model.deploy(
         traffic_split={"0": 100},
-        machine_type='n1-standard-2',
+        machine_type="n1-standard-2",
     )
--- notebooks/kubeflow_pipelines/cicd/solutions/trainer_image_vertex/train.py
+++ notebooks/kubeflow_pipelines/cicd/solutions/trainer_image_vertex/train.py
@@ -23,15 +23,15 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 AIP_MODEL_DIR = os.environ["AIP_MODEL_DIR"]
-MODEL_FILENAME = 'model.pkl'
+MODEL_FILENAME = "model.pkl"
 
 
-def train_evaluate(training_dataset_path, validation_dataset_path, alpha, max_iter, hptune):
+def train_evaluate(
+    training_dataset_path, validation_dataset_path, alpha, max_iter, hptune
+):
     """Trains the Covertype Classifier model."""
 
     df_train = pd.read_csv(training_dataset_path)
@@ -41,50 +41,65 @@ def train_evaluate(training_dataset_path, validation_dataset_path, alpha, max_it
         df_train = pd.concat([df_train, df_validation])
 
     numeric_features = [
-        'Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology',
-        'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways',
-        'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm',
-        'Horizontal_Distance_To_Fire_Points'
+        "Elevation",
+        "Aspect",
+        "Slope",
+        "Horizontal_Distance_To_Hydrology",
+        "Vertical_Distance_To_Hydrology",
+        "Horizontal_Distance_To_Roadways",
+        "Hillshade_9am",
+        "Hillshade_Noon",
+        "Hillshade_3pm",
+        "Horizontal_Distance_To_Fire_Points",
     ]
 
-    categorical_features = ['Wilderness_Area', 'Soil_Type']
+    categorical_features = ["Wilderness_Area", "Soil_Type"]
 
-    preprocessor = ColumnTransformer(transformers=[
-        ('num', StandardScaler(), numeric_features),
-        ('cat', OneHotEncoder(), categorical_features)])
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", StandardScaler(), numeric_features),
+            ("cat", OneHotEncoder(), categorical_features),
+        ]
+    )
 
-    pipeline = Pipeline([('preprocessor', preprocessor),
-                         ('classifier', SGDClassifier(loss='log'))])
+    pipeline = Pipeline(
+        [
+            ("preprocessor", preprocessor),
+            ("classifier", SGDClassifier(loss="log")),
+        ]
+    )
 
-    num_features_type_map = {feature: 'float64' for feature in numeric_features}
+    num_features_type_map = {feature: "float64" for feature in numeric_features}
     df_train = df_train.astype(num_features_type_map)
     df_validation = df_validation.astype(num_features_type_map)
 
-    print('Starting training: alpha={}, max_iter={}'.format(alpha, max_iter))
-    X_train = df_train.drop('Cover_Type', axis=1)
-    y_train = df_train['Cover_Type']
+    print(f"Starting training: alpha={alpha}, max_iter={max_iter}")
+    X_train = df_train.drop("Cover_Type", axis=1)
+    y_train = df_train["Cover_Type"]
 
     pipeline.set_params(classifier__alpha=alpha, classifier__max_iter=max_iter)
     pipeline.fit(X_train, y_train)
 
     if hptune:
-        X_validation = df_validation.drop('Cover_Type', axis=1)
-        y_validation = df_validation['Cover_Type']
+        X_validation = df_validation.drop("Cover_Type", axis=1)
+        y_validation = df_validation["Cover_Type"]
         accuracy = pipeline.score(X_validation, y_validation)
-        print('Model accuracy: {}'.format(accuracy))
+        print(f"Model accuracy: {accuracy}")
         # Log it with hypertune
         hpt = hypertune.HyperTune()
         hpt.report_hyperparameter_tuning_metric(
-            hyperparameter_metric_tag='accuracy', metric_value=accuracy)
+            hyperparameter_metric_tag="accuracy", metric_value=accuracy
+        )
 
     # Save the model
     if not hptune:
-        with open(MODEL_FILENAME, 'wb') as model_file:
+        with open(MODEL_FILENAME, "wb") as model_file:
             pickle.dump(pipeline, model_file)
-        subprocess.check_call(['gsutil', 'cp', MODEL_FILENAME, AIP_MODEL_DIR],
-                              stderr=sys.stdout)
-        print(f'Saved model in: {AIP_MODEL_DIR}')
+        subprocess.check_call(
+            ["gsutil", "cp", MODEL_FILENAME, AIP_MODEL_DIR], stderr=sys.stdout
+        )
+        print(f"Saved model in: {AIP_MODEL_DIR}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fire.Fire(train_evaluate)

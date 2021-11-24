7e21ad59191cc505580601164c3086ec50f1e929
Daniel Sparing <dsparing@google.com>
2021-11-24 23:13:59 +0000

    kf-nbqa-longdiffs

--- notebooks/kubeflow_pipelines/cicd/labs/pipeline/helper_components.py
+++ notebooks/kubeflow_pipelines/cicd/labs/pipeline/helper_components.py
@@ -17,81 +17,85 @@ from typing import NamedTuple
 
 def retrieve_best_run(
     project_id: str, job_id: str
-) -> NamedTuple('Outputs', [('metric_value', float), ('alpha', float),
-                            ('max_iter', int)]):
-  """Retrieves the parameters of the best Hypertune run."""
+) -> NamedTuple(
+    "Outputs", [("metric_value", float), ("alpha", float), ("max_iter", int)]
+):
+    """Retrieves the parameters of the best Hypertune run."""
 
-  from googleapiclient import discovery
-  from googleapiclient import errors
+    from googleapiclient import discovery, errors
 
-  ml = discovery.build('ml', 'v1')
+    ml = discovery.build("ml", "v1")
 
-  job_name = 'projects/{}/jobs/{}'.format(project_id, job_id)
-  request = ml.projects().jobs().get(name=job_name)
+    job_name = f"projects/{project_id}/jobs/{job_id}"
+    request = ml.projects().jobs().get(name=job_name)
 
-  try:
-    response = request.execute()
-  except errors.HttpError as err:
-    print(err)
-  except:
-    print('Unexpected error')
+    try:
+        response = request.execute()
+    except errors.HttpError as err:
+        print(err)
+    except:
+        print("Unexpected error")
 
-  print(response)
+    print(response)
 
-  best_trial = response['trainingOutput']['trials'][0]
+    best_trial = response["trainingOutput"]["trials"][0]
 
-  metric_value = best_trial['finalMetric']['objectiveValue']
-  alpha = float(best_trial['hyperparameters']['alpha'])
-  max_iter = int(best_trial['hyperparameters']['max_iter'])
+    metric_value = best_trial["finalMetric"]["objectiveValue"]
+    alpha = float(best_trial["hyperparameters"]["alpha"])
+    max_iter = int(best_trial["hyperparameters"]["max_iter"])
 
-  return (metric_value, alpha, max_iter)
+    return (metric_value, alpha, max_iter)
 
 
 def evaluate_model(
     dataset_path: str, model_path: str, metric_name: str
-) -> NamedTuple('Outputs', [('metric_name', str), ('metric_value', float),
-                            ('mlpipeline_metrics', 'Metrics')]):
-  """Evaluates a trained sklearn model."""
-  #import joblib
-  import pickle
-  import json
-  import pandas as pd
-  import subprocess
-  import sys
+) -> NamedTuple(
+    "Outputs",
+    [
+        ("metric_name", str),
+        ("metric_value", float),
+        ("mlpipeline_metrics", "Metrics"),
+    ],
+):
+    """Evaluates a trained sklearn model."""
+    # import joblib
+    import json
+    import pickle
+    import subprocess
+    import sys
 
-  from sklearn.metrics import accuracy_score, recall_score
+    import pandas as pd
+    from sklearn.metrics import accuracy_score, recall_score
 
-  df_test = pd.read_csv(dataset_path)
+    df_test = pd.read_csv(dataset_path)
 
-  X_test = df_test.drop('Cover_Type', axis=1)
-  y_test = df_test['Cover_Type']
+    X_test = df_test.drop("Cover_Type", axis=1)
+    y_test = df_test["Cover_Type"]
 
-  # Copy the model from GCS
-  model_filename = 'model.pkl'
-  gcs_model_filepath = '{}/{}'.format(model_path, model_filename)
-  print(gcs_model_filepath)
-  subprocess.check_call(['gsutil', 'cp', gcs_model_filepath, model_filename],
-                        stderr=sys.stdout)
+    # Copy the model from GCS
+    model_filename = "model.pkl"
+    gcs_model_filepath = f"{model_path}/{model_filename}"
+    print(gcs_model_filepath)
+    subprocess.check_call(
+        ["gsutil", "cp", gcs_model_filepath, model_filename], stderr=sys.stdout
+    )
 
-  with open(model_filename, 'rb') as model_file:
-    model = pickle.load(model_file)
+    with open(model_filename, "rb") as model_file:
+        model = pickle.load(model_file)
 
-  y_hat = model.predict(X_test)
+    y_hat = model.predict(X_test)
 
-  if metric_name == 'accuracy':
-    metric_value = accuracy_score(y_test, y_hat)
-  elif metric_name == 'recall':
-    metric_value = recall_score(y_test, y_hat)
-  else:
-    metric_name = 'N/A'
-    metric_value = 0
+    if metric_name == "accuracy":
+        metric_value = accuracy_score(y_test, y_hat)
+    elif metric_name == "recall":
+        metric_value = recall_score(y_test, y_hat)
+    else:
+        metric_name = "N/A"
+        metric_value = 0
 
-  # Export the metric
-  metrics = {
-      'metrics': [{
-          'name': metric_name,
-          'numberValue': float(metric_value)
-      }]
-  }
+    # Export the metric
+    metrics = {
+        "metrics": [{"name": metric_name, "numberValue": float(metric_value)}]
+    }
 
-  return (metric_name, metric_value, json.dumps(metrics))
+    return (metric_name, metric_value, json.dumps(metrics))

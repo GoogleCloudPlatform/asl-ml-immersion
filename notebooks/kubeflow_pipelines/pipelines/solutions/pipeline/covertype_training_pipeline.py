56ab00c603e725f5ac08907d2cf33d449158fe3a
Daniel Sparing <dsparing@google.com>
2021-11-24 21:04:46 +0000

    linting

--- notebooks/kubeflow_pipelines/pipelines/solutions/pipeline/covertype_training_pipeline.py
+++ notebooks/kubeflow_pipelines/pipelines/solutions/pipeline/covertype_training_pipeline.py
@@ -15,32 +15,27 @@
 
 import os
 
-from helper_components import evaluate_model
-from helper_components import retrieve_best_run
-from jinja2 import Template
 import kfp
+from helper_components import evaluate_model, retrieve_best_run
+from jinja2 import Template
 from kfp.components import func_to_container_op
-from kfp.dsl.types import Dict
-from kfp.dsl.types import GCPProjectID
-from kfp.dsl.types import GCPRegion
-from kfp.dsl.types import GCSPath
-from kfp.dsl.types import String
+from kfp.dsl.types import Dict, GCPProjectID, GCPRegion, GCSPath, String
 from kfp.gcp import use_gcp_secret
 
 # Defaults and environment settings
-BASE_IMAGE = os.getenv('BASE_IMAGE')
-TRAINER_IMAGE = os.getenv('TRAINER_IMAGE')
-RUNTIME_VERSION = os.getenv('RUNTIME_VERSION')
-PYTHON_VERSION = os.getenv('PYTHON_VERSION')
-COMPONENT_URL_SEARCH_PREFIX = os.getenv('COMPONENT_URL_SEARCH_PREFIX')
-USE_KFP_SA = os.getenv('USE_KFP_SA')
+BASE_IMAGE = os.getenv("BASE_IMAGE")
+TRAINER_IMAGE = os.getenv("TRAINER_IMAGE")
+RUNTIME_VERSION = os.getenv("RUNTIME_VERSION")
+PYTHON_VERSION = os.getenv("PYTHON_VERSION")
+COMPONENT_URL_SEARCH_PREFIX = os.getenv("COMPONENT_URL_SEARCH_PREFIX")
+USE_KFP_SA = os.getenv("USE_KFP_SA")
 
-TRAINING_FILE_PATH = 'datasets/training/data.csv'
-VALIDATION_FILE_PATH = 'datasets/validation/data.csv'
-TESTING_FILE_PATH = 'datasets/testing/data.csv'
+TRAINING_FILE_PATH = "datasets/training/data.csv"
+VALIDATION_FILE_PATH = "datasets/validation/data.csv"
+TESTING_FILE_PATH = "datasets/testing/data.csv"
 
 # Parameter defaults
-SPLITS_DATASET_ID = 'splits'
+SPLITS_DATASET_ID = "splits"
 HYPERTUNE_SETTINGS = """
 {
     "hyperparameters":  {
@@ -80,93 +75,107 @@ def generate_sampling_query(source_table_name, num_lots, lots):
          MOD(ABS(FARM_FINGERPRINT(TO_JSON_STRING(cover))), {{ num_lots }}) IN ({{ lots }})
          """
     query = Template(sampling_query_template).render(
-        source_table=source_table_name, num_lots=num_lots, lots=str(lots)[1:-1])
+        source_table=source_table_name, num_lots=num_lots, lots=str(lots)[1:-1]
+    )
 
     return query
 
 
 # Create component factories
 component_store = kfp.components.ComponentStore(
-    local_search_paths=None, url_search_prefixes=[COMPONENT_URL_SEARCH_PREFIX])
+    local_search_paths=None, url_search_prefixes=[COMPONENT_URL_SEARCH_PREFIX]
+)
 
-bigquery_query_op = component_store.load_component('bigquery/query')
-mlengine_train_op = component_store.load_component('ml_engine/train')
-mlengine_deploy_op = component_store.load_component('ml_engine/deploy')
+bigquery_query_op = component_store.load_component("bigquery/query")
+mlengine_train_op = component_store.load_component("ml_engine/train")
+mlengine_deploy_op = component_store.load_component("ml_engine/deploy")
 retrieve_best_run_op = func_to_container_op(
-    retrieve_best_run, base_image=BASE_IMAGE)
+    retrieve_best_run, base_image=BASE_IMAGE
+)
 evaluate_model_op = func_to_container_op(evaluate_model, base_image=BASE_IMAGE)
 
 
 @kfp.dsl.pipeline(
-    name='Covertype Classifier Training',
-    description='The pipeline training and deploying the Covertype classifierpipeline_yaml'
+    name="Covertype Classifier Training",
+    description="The pipeline training and deploying the Covertype classifierpipeline_yaml",
 )
-def covertype_train(project_id,
-                    region,
-                    source_table_name,
-                    gcs_root,
-                    dataset_id,
-                    evaluation_metric_name,
-                    evaluation_metric_threshold,
-                    model_id,
-                    version_id,
-                    replace_existing_version,
-                    hypertune_settings=HYPERTUNE_SETTINGS,
-                    dataset_location='US'):
+def covertype_train(
+    project_id,
+    region,
+    source_table_name,
+    gcs_root,
+    dataset_id,
+    evaluation_metric_name,
+    evaluation_metric_threshold,
+    model_id,
+    version_id,
+    replace_existing_version,
+    hypertune_settings=HYPERTUNE_SETTINGS,
+    dataset_location="US",
+):
     """Orchestrates training and deployment of an sklearn model."""
 
     # Create the training split
     query = generate_sampling_query(
-        source_table_name=source_table_name, num_lots=10, lots=[1, 2, 3, 4])
+        source_table_name=source_table_name, num_lots=10, lots=[1, 2, 3, 4]
+    )
 
-    training_file_path = '{}/{}'.format(gcs_root, TRAINING_FILE_PATH)
+    training_file_path = f"{gcs_root}/{TRAINING_FILE_PATH}"
 
     create_training_split = bigquery_query_op(
         query=query,
         project_id=project_id,
         dataset_id=dataset_id,
-        table_id='',
+        table_id="",
         output_gcs_path=training_file_path,
-        dataset_location=dataset_location)
+        dataset_location=dataset_location,
+    )
 
     # Create the validation split
     query = generate_sampling_query(
-        source_table_name=source_table_name, num_lots=10, lots=[8])
+        source_table_name=source_table_name, num_lots=10, lots=[8]
+    )
 
-    validation_file_path = '{}/{}'.format(gcs_root, VALIDATION_FILE_PATH)
+    validation_file_path = f"{gcs_root}/{VALIDATION_FILE_PATH}"
 
     create_validation_split = bigquery_query_op(
         query=query,
         project_id=project_id,
         dataset_id=dataset_id,
-        table_id='',
+        table_id="",
         output_gcs_path=validation_file_path,
-        dataset_location=dataset_location)
+        dataset_location=dataset_location,
+    )
 
     # Create the testing split
     query = generate_sampling_query(
-        source_table_name=source_table_name, num_lots=10, lots=[9])
+        source_table_name=source_table_name, num_lots=10, lots=[9]
+    )
 
-    testing_file_path = '{}/{}'.format(gcs_root, TESTING_FILE_PATH)
+    testing_file_path = f"{gcs_root}/{TESTING_FILE_PATH}"
 
     create_testing_split = bigquery_query_op(
         query=query,
         project_id=project_id,
         dataset_id=dataset_id,
-        table_id='',
+        table_id="",
         output_gcs_path=testing_file_path,
-        dataset_location=dataset_location)
+        dataset_location=dataset_location,
+    )
 
     # Tune hyperparameters
     tune_args = [
-        '--training_dataset_path',
-        create_training_split.outputs['output_gcs_path'],
-        '--validation_dataset_path',
-        create_validation_split.outputs['output_gcs_path'], '--hptune', 'True'
+        "--training_dataset_path",
+        create_training_split.outputs["output_gcs_path"],
+        "--validation_dataset_path",
+        create_validation_split.outputs["output_gcs_path"],
+        "--hptune",
+        "True",
     ]
 
-    job_dir = '{}/{}/{}'.format(gcs_root, 'jobdir/hypertune',
-                                kfp.dsl.RUN_ID_PLACEHOLDER)
+    job_dir = "{}/{}/{}".format(
+        gcs_root, "jobdir/hypertune", kfp.dsl.RUN_ID_PLACEHOLDER
+    )
 
     hypertune = mlengine_train_op(
         project_id=project_id,
@@ -174,22 +183,28 @@ def covertype_train(project_id,
         master_image_uri=TRAINER_IMAGE,
         job_dir=job_dir,
         args=tune_args,
-        training_input=hypertune_settings)
+        training_input=hypertune_settings,
+    )
 
     # Retrieve the best trial
     get_best_trial = retrieve_best_run_op(
-            project_id, hypertune.outputs['job_id'])
+        project_id, hypertune.outputs["job_id"]
+    )
 
     # Train the model on a combined training and validation datasets
-    job_dir = '{}/{}/{}'.format(gcs_root, 'jobdir', kfp.dsl.RUN_ID_PLACEHOLDER)
+    job_dir = "{}/{}/{}".format(gcs_root, "jobdir", kfp.dsl.RUN_ID_PLACEHOLDER)
 
     train_args = [
-        '--training_dataset_path',
-        create_training_split.outputs['output_gcs_path'],
-        '--validation_dataset_path',
-        create_validation_split.outputs['output_gcs_path'], '--alpha',
-        get_best_trial.outputs['alpha'], '--max_iter',
-        get_best_trial.outputs['max_iter'], '--hptune', 'False'
+        "--training_dataset_path",
+        create_training_split.outputs["output_gcs_path"],
+        "--validation_dataset_path",
+        create_validation_split.outputs["output_gcs_path"],
+        "--alpha",
+        get_best_trial.outputs["alpha"],
+        "--max_iter",
+        get_best_trial.outputs["max_iter"],
+        "--hptune",
+        "False",
     ]
 
     train_model = mlengine_train_op(
@@ -197,27 +212,33 @@ def covertype_train(project_id,
         region=region,
         master_image_uri=TRAINER_IMAGE,
         job_dir=job_dir,
-        args=train_args)
+        args=train_args,
+    )
 
     # Evaluate the model on the testing split
     eval_model = evaluate_model_op(
-        dataset_path=str(create_testing_split.outputs['output_gcs_path']),
-        model_path=str(train_model.outputs['job_dir']),
-        metric_name=evaluation_metric_name)
+        dataset_path=str(create_testing_split.outputs["output_gcs_path"]),
+        model_path=str(train_model.outputs["job_dir"]),
+        metric_name=evaluation_metric_name,
+    )
 
     # Deploy the model if the primary metric is better than threshold
-    with kfp.dsl.Condition(eval_model.outputs['metric_value'] > evaluation_metric_threshold):
+    with kfp.dsl.Condition(
+        eval_model.outputs["metric_value"] > evaluation_metric_threshold
+    ):
         deploy_model = mlengine_deploy_op(
-        model_uri=train_model.outputs['job_dir'],
-        project_id=project_id,
-        model_id=model_id,
-        version_id=version_id,
-        runtime_version=RUNTIME_VERSION,
-        python_version=PYTHON_VERSION,
-        replace_existing_version=replace_existing_version)
+            model_uri=train_model.outputs["job_dir"],
+            project_id=project_id,
+            model_id=model_id,
+            version_id=version_id,
+            runtime_version=RUNTIME_VERSION,
+            python_version=PYTHON_VERSION,
+            replace_existing_version=replace_existing_version,
+        )
 
     # Configure the pipeline to run using the service account defined
     # in the user-gcp-sa k8s secret
-    if USE_KFP_SA == 'True':
+    if USE_KFP_SA == "True":
         kfp.dsl.get_pipeline_conf().add_op_transformer(
-              use_gcp_secret('user-gcp-sa'))
+            use_gcp_secret("user-gcp-sa")
+        )
--- notebooks/kubeflow_pipelines/pipelines/solutions/pipeline_vertex/pipeline.py
+++ notebooks/kubeflow_pipelines/pipelines/solutions/pipeline_vertex/pipeline.py
@@ -15,40 +15,37 @@ import os
 
 from kfp import dsl
 from kfp.components import create_component_from_func_v2
-
-from tuning_lightweight_component import tune_hyperparameters
 from training_lightweight_component import train_and_deploy
+from tuning_lightweight_component import tune_hyperparameters
 
+PIPELINE_ROOT = os.getenv("PIPELINE_ROOT")
+PROJECT_ID = os.getenv("PROJECT_ID")
+REGION = os.getenv("REGION")
 
-PIPELINE_ROOT = os.getenv('PIPELINE_ROOT')
-PROJECT_ID = os.getenv('PROJECT_ID')
-REGION = os.getenv('REGION')
+TRAINING_CONTAINER_IMAGE_URI = os.getenv("TRAINING_CONTAINER_IMAGE_URI")
+SERVING_CONTAINER_IMAGE_URI = os.getenv("SERVING_CONTAINER_IMAGE_URI")
 
-TRAINING_CONTAINER_IMAGE_URI = os.getenv('TRAINING_CONTAINER_IMAGE_URI')
-SERVING_CONTAINER_IMAGE_URI = os.getenv('SERVING_CONTAINER_IMAGE_URI')
+TRAINING_FILE_PATH = os.getenv("TRAINING_FILE_PATH")
+VALIDATION_FILE_PATH = os.getenv("VALIDATION_FILE_PATH")
 
-TRAINING_FILE_PATH = os.getenv('TRAINING_FILE_PATH')
-VALIDATION_FILE_PATH = os.getenv('VALIDATION_FILE_PATH')
-
-MAX_TRIAL_COUNT = os.getenv('MAX_TRIAL_COUNT', 5)
-PARALLEL_TRIAL_COUNT = os.getenv('PARALLEL_TRIAL_COUNT', 5)
-THRESHOLD = os.getenv('THRESHOLD', 0.6)
+MAX_TRIAL_COUNT = os.getenv("MAX_TRIAL_COUNT", 5)
+PARALLEL_TRIAL_COUNT = os.getenv("PARALLEL_TRIAL_COUNT", 5)
+THRESHOLD = os.getenv("THRESHOLD", 0.6)
 
 
 tune_hyperparameters_component = create_component_from_func_v2(
     tune_hyperparameters,
-    base_image='python:3.8',
-    output_component_file='covertype_kfp_tune_hyperparameters.yaml',
-    packages_to_install=['google-cloud-aiplatform'],
+    base_image="python:3.8",
+    output_component_file="covertype_kfp_tune_hyperparameters.yaml",
+    packages_to_install=["google-cloud-aiplatform"],
 )
 
 
 train_and_deploy_component = create_component_from_func_v2(
     train_and_deploy,
-    base_image='python:3.8',
-    output_component_file='covertype_kfp_train_and_deploy.yaml',
-    packages_to_install=['google-cloud-aiplatform'],
-
+    base_image="python:3.8",
+    output_component_file="covertype_kfp_train_and_deploy.yaml",
+    packages_to_install=["google-cloud-aiplatform"],
 )
 
 
@@ -67,7 +64,7 @@ def covertype_train(
     parallel_trial_count: int = PARALLEL_TRIAL_COUNT,
     pipeline_root: str = PIPELINE_ROOT,
 ):
-    staging_bucket = f'{pipeline_root}/staging'
+    staging_bucket = f"{pipeline_root}/staging"
 
     tuning_op = tune_hyperparameters_component(
         project=PROJECT_ID,
@@ -80,9 +77,11 @@ def covertype_train(
         parallel_trial_count=parallel_trial_count,
     )
 
-    accuracy = tuning_op.outputs['best_accuracy']
+    accuracy = tuning_op.outputs["best_accuracy"]
 
-    with dsl.Condition(accuracy >= accuracy_deployment_threshold, name="deploy_decision"):
+    with dsl.Condition(
+        accuracy >= accuracy_deployment_threshold, name="deploy_decision"
+    ):
         train_and_deploy_op = train_and_deploy_component(
             project=PROJECT_ID,
             location=REGION,
@@ -91,6 +90,6 @@ def covertype_train(
             training_file_path=training_file_path,
             validation_file_path=validation_file_path,
             staging_bucket=staging_bucket,
-            alpha=tuning_op.outputs['best_alpha'],
-            max_iter=tuning_op.outputs['best_max_iter'],
+            alpha=tuning_op.outputs["best_alpha"],
+            max_iter=tuning_op.outputs["best_max_iter"],
         )

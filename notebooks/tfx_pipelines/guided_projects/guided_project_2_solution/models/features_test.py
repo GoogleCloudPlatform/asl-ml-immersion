8c6dd79d1822a08f98d059b02436ad932c7687de
Daniel Sparing <dsparing@google.com>
2021-12-02 21:00:50 +0000

    pylint

--- notebooks/tfx_pipelines/guided_projects/guided_project_2_solution/models/features_test.py
+++ notebooks/tfx_pipelines/guided_projects/guided_project_2_solution/models/features_test.py
@@ -1,5 +1,4 @@
-# Lint as: python2, python3
-# Copyright 2020 Google LLC. All Rights Reserved.
+# Copyright 2021 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Features Test."""
 
 
 import tensorflow as tf

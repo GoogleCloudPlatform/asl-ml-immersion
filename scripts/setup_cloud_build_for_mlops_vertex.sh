c7cc87c73253d8f4e418b4bfce37b7d99f0b1c46
Benoit Dherin <dherin@google.com>
2021-12-17 14:10:11 +0000

    add service enabling in mlops setup

--- scripts/setup_cloud_build_for_mlops_vertex.sh
+++ scripts/setup_cloud_build_for_mlops_vertex.sh
@@ -24,3 +24,12 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com \
     --role roles/storage.objectAdmin
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com \
+    --role roles/aiplatform.user
+
+gcloud iam service-accounts add-iam-policy-binding \
+    $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+    --member serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com \
+    --role roles/iam.serviceAccountUser

3b7913f84fe5fd336763d96d5d4cdea8dc8ca947
Takumi Ohyama <takumiohym@google.com>
2022-07-07 11:14:31 +0000

    reflected feedbacks + updated modle.py and task.py

--- notebooks/building_production_ml_systems/solutions/taxifare/trainer/task.py
+++ notebooks/building_production_ml_systems/solutions/taxifare/trainer/task.py
@@ -20,9 +20,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--nnsize",
         help="Hidden layer sizes (provide space-separated sizes)",
-        nargs="+",
-        type=int,
-        default=[32, 8],
+        default="32 8",
     )
     parser.add_argument(
         "--nbuckets",
@@ -55,13 +53,7 @@ if __name__ == "__main__":
         help="GCS location pattern of train files containing eval URLs",
         required=True,
     )
-    parser.add_argument(
-        "--job-dir",
-        help="this model ignores this field, but it is required by gcloud",
-        default="junk",
-    )
     args = parser.parse_args()
     hparams = args.__dict__
-    hparams.pop("job-dir", None)
 
     model.train_and_evaluate(hparams)

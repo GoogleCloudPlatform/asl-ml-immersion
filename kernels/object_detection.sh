ee0f698a436354b02d4be8dedeb3c67697920588
Takumi Ohyama <takumiohym@google.com>
2022-12-09 22:45:15 +0000

    fixed object_detection.sh

--- kernels/object_detection.sh
+++ kernels/object_detection.sh
@@ -3,20 +3,19 @@ ENVNAME=object_detection_kernel
 
 cd ~/asl-ml-immersion/notebooks/image_models/solutions
 
-# Setup virtual env and kernel
-python3 -m venv $ENVNAME --system-site-packages
-source $ENVNAME/bin/activate
-python -m ipykernel install --user --name=$ENVNAME
-
 # Download the Object Detection API
 git clone --depth 1 https://github.com/tensorflow/models
 cd models/research/
 protoc object_detection/protos/*.proto --python_out=.
 cp object_detection/packages/tf2/setup.py .
 
+# Setup virtual env and kernel
+python3 -m venv $ENVNAME --system-site-packages
+source $ENVNAME/bin/activate
+python -m ipykernel install --user --name=$ENVNAME
+
 # Install Object Detection API and its dependencies
-pip install --user .
-pip install --user protobuf==3.20
-pip install --user numpy --force-reinstall
+pip install -U pip
+pip install .
 
 deactivate

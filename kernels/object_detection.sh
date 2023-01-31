3a58334e5dbef5d1fde89e1ce988124ba447925e
Takumi Ohyama <takumiohym@google.com>
2023-01-31 18:51:47 +0000

    FIX: fixed the object_detectio kernel command due to the protobuf breaking change

--- kernels/object_detection.sh
+++ kernels/object_detection.sh
@@ -34,4 +34,8 @@ python -m ipykernel install --user --name=$ENVNAME
 pip install -q -U pip
 pip install -q .
 
+# Add the missing builer.py file due to the breaking change.
+# https://stackoverflow.com/questions/71759248/importerror-cannot-import-name-builder-from-google-protobuf-internal
+wget https://raw.githubusercontent.com/protocolbuffers/protobuf/main/python/google/protobuf/internal/builder.py -O ./$ENVNAME/lib/python3.7/site-packages/google/protobuf/internal/builder.py
+
 deactivate

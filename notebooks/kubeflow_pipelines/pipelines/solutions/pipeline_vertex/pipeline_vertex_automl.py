568a319e71dcd724b3e6c5501155a644a8590f31
Takumi Ohyama <takumiohym@google.com>
2024-04-03 13:15:03 +0000

    Update KFP AutoML Online pipeline

--- notebooks/kubeflow_pipelines/pipelines/solutions/pipeline_vertex/pipeline_vertex_automl.py
+++ notebooks/kubeflow_pipelines/pipelines/solutions/pipeline_vertex/pipeline_vertex_automl.py
@@ -16,13 +16,15 @@
 
 import os
 
-from google_cloud_pipeline_components.aiplatform import (
+from google_cloud_pipeline_components.v1.automl.training_job import (
     AutoMLTabularTrainingJobRunOp,
+)
+from google_cloud_pipeline_components.v1.dataset import TabularDatasetCreateOp
+from google_cloud_pipeline_components.v1.endpoint import (
     EndpointCreateOp,
     ModelDeployOp,
-    TabularDatasetCreateOp,
 )
-from kfp.v2 import dsl
+from kfp import dsl
 
 PIPELINE_ROOT = os.getenv("PIPELINE_ROOT")
 PROJECT = os.getenv("PROJECT")
@@ -39,6 +41,7 @@ SERVING_MACHINE_TYPE = os.getenv("SERVING_MACHINE_TYPE", "n1-standard-16")
     pipeline_root=PIPELINE_ROOT,
 )
 def create_pipeline():
+
     dataset_create_task = TabularDatasetCreateOp(
         display_name=DISPLAY_NAME,
         bq_source=DATASET_SOURCE,

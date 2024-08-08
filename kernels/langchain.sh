f529130e7877f8c4d8ab3c9cbd2d7a2938db8474
Sanjana Reddy <sanjanalreddy@google.com>
2024-08-08 19:40:29 +0000

    update langchain kernel

--- kernels/langchain.sh
+++ kernels/langchain.sh
@@ -25,13 +25,18 @@ source $ENVNAME/bin/activate
 python -m ipykernel install --user --name=$ENVNAME
 
 pip install -q -U pip
-pip install langchain==0.0.323
-pip install langchain-google-vertexai==1.0.1
-pip install chromadb==0.3.26
-pip install pydantic==1.10.8
-pip install google-cloud-aiplatform==1.48.0
-pip install faiss-cpu==1.7.4
-pip install unstructured==0.14.4
-pip install wikipedia==1.4.0
+pip install -q -U langchain==0.2.12
+pip install -q -U langchain-chroma==0.1.2
+pip install -q -U langchain-community==0.2.11
+pip install -q -U langchain-core==0.2.29
+pip install -q -U langchain-google-vertexai==1.0.8
+pip install -q -U langchain-text-splitters==0.2.2
+pip install -q -U chromadb==0.3.26
+pip install -q -U pydantic==1.10.8
+pip install -q -U pypdf==4.3.1
+pip install -q -U google-cloud-aiplatform==1.58.0
+pip install -q -U faiss-cpu==1.7.4
+pip install -q -U unstructured==0.14.4
+pip install -q -U wikipedia==1.4.0
 
 deactivate

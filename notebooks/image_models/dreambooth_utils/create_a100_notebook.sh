86ee6202ebf64009ebd9514ea162d2c8c299e57c
Takumi Ohyama <takumiohym@google.com>
2025-01-02 14:53:02 +0000

    Update a100 instance creation command to workbench instance

--- notebooks/image_models/dreambooth_utils/create_a100_notebook.sh
+++ notebooks/image_models/dreambooth_utils/create_a100_notebook.sh
@@ -1,14 +1,18 @@
-ZONE=us-central1-a
 MACHINE_NAME=a100-notebook
+ZONE=us-central1-a
 MACHINE_TYPE=a2-highgpu-1g
-IMAGE_FAMILY=tf-ent-2-12-gpu-debian-11-py310
+IMAGE_PROJECT=cloud-notebooks-managed
+IMAGE_FAMILY=workbench-instances
+DISC_TYPE=PD_STANDARD
 
-while ! gcloud notebooks instances create $MACHINE_NAME \
-    --vm-image-project=deeplearning-platform-release \
+while ! gcloud workbench instances create $MACHINE_NAME \
+    --vm-image-project=$IMAGE_PROJECT \
     --vm-image-family=$IMAGE_FAMILY \
     --machine-type=$MACHINE_TYPE \
-    --location=$ZONE \
-    --install-gpu-driver
+    --install-gpu-driver \
+    --boot-disk-type=$DISC_TYPE \
+    --data-disk-type=$DISC_TYPE \
+    --location=$ZONE
 do
     echo 'retry instance creation'
     sleep 5

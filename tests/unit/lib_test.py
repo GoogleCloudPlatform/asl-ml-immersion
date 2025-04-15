252bc3b980479ec5915b638b0ef2bcf5730c8d4a
Takumi Ohyama <takumiohym@google.com>
2025-04-15 18:02:54 +0000

    Remove tests

--- tests/unit/lib_test.py
+++
@@ -1,9 +0,0 @@
-"""Unit tests for lib.py"""
-
-from asl import lib
-
-
-def test_extract_relative_path():
-    test_root_path = "/tmp/a"
-    test_path = "/tmp/a/b"
-    assert "a/b" == lib.extract_relative_path(test_root_path, test_path)

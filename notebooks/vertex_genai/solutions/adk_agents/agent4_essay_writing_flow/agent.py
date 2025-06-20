70d7ac3670ff27dc7db85e7fb255ec5563bd9c82
Takumi Ohyama <takumiohym@google.com>
2025-06-20 17:15:54 +0000

    Update Google Search tool usage

--- notebooks/vertex_genai/solutions/adk_agents/agent4_essay_writing_flow/agent.py
+++ notebooks/vertex_genai/solutions/adk_agents/agent4_essay_writing_flow/agent.py
@@ -1,4 +1,5 @@
 # pylint: skip-file
+
 import os
 
 import google.adk as adk
@@ -14,11 +15,10 @@ PLAN_PROMPT = """You are an expert writer tasked with writing a high level outli
 Write such an outline for the user provided topic. Give an outline of the essay along with any
 relevant notes or instructions for the sections."""
 
+
 INITIAL_WRITER_PROMPT = """
 You are a essay write tasked with writing excellent 3-pages essays.
-First, based on the topic and outline from the user,
-create search queries, run the search tool to get information, and collect the information.
-Next, generate the best essay possible using the initial request, the outline, and the research provided.
+Generate the best essay possible using google search, based on the outline.
 If critique is provided below, respond with a revised version of your previous attempts.
 Use Markdown formatting to specify a title and section headers for each paragraph.
 Utilize all of the information below as needed:
@@ -27,6 +27,7 @@ Plan:
 {plan}
 
 """
+
 
 WRITER_PROMPT = (
     INITIAL_WRITER_PROMPT
@@ -51,22 +52,12 @@ planner_agent = LlmAgent(
     output_key="plan",
 )
 
-search_agent = LlmAgent(
-    name="basic_search_agent",
-    model=MODEL,
-    description="Agent to answer questions using Google Search.",
-    instruction="I can answer your questions by searching the internet. Just ask me anything!",
-    # google_search is a pre-built tool which allows the agent to perform Google searches.
-    tools=[google_search],
-)
-
-
 # Agent to write the essay, using context from the conversation
 initial_writer_agent = LlmAgent(
     name="initial_writer_agent",
     model=MODEL,
     instruction=INITIAL_WRITER_PROMPT,
-    tools=[AgentTool(search_agent)],
+    tools=[google_search],
     output_key="draft",
 )
 
@@ -84,7 +75,7 @@ writer_agent = LlmAgent(
     name="writer_agent",
     model=MODEL,
     instruction=WRITER_PROMPT,
-    tools=[AgentTool(search_agent)],
+    tools=[google_search],
     output_key="draft",
 )
 
@@ -92,7 +83,7 @@ writer_agent = LlmAgent(
 refinement_loop = LoopAgent(
     name="refinement_loop",
     sub_agents=[reflection_agent, writer_agent],
-    max_iterations=3,
+    max_iterations=2,
 )
 
 # The main sequential agent that orchestrates the entire process
@@ -100,7 +91,6 @@ essay_writing_agent = SequentialAgent(
     name="essay_writing_agent",
     sub_agents=[
         planner_agent,
-        # research_agent,
         initial_writer_agent,
         refinement_loop,
     ],

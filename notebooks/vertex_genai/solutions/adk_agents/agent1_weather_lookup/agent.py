c4c4f831e8e9841a1eb65392c2deacf7afd2deb8
olex-snk <osaienko@google.com>
2025-07-22 09:57:45 +0100

    revert_style changes

--- notebooks/vertex_genai/solutions/adk_agents/agent1_weather_lookup/agent.py
+++ notebooks/vertex_genai/solutions/adk_agents/agent1_weather_lookup/agent.py
@@ -1,16 +1,18 @@
+# pylint: skip-file
 from google.adk.agents import Agent
+
 MODEL = "gemini-2.0-flash"
 
 from .tools import get_weather
 
 root_agent = Agent(
     name="weather_agent_v1",
-    model=MODEL, # Can be a string for Gemini or a LiteLlm object
+    model=MODEL,  # Can be a string for Gemini or a LiteLlm object
     description="Provides weather information for specific cities.",
     instruction="You are a helpful weather assistant. "
-                "When the user asks for the weather in a specific city, "
-                "use the 'get_weather' tool to find the information. "
-                "If the tool returns an error, inform the user politely. "
-                "If the tool is successful, present the weather report clearly.",
-    tools=[get_weather], # Pass the function directly
+    "When the user asks for the weather in a specific city, "
+    "use the 'get_weather' tool to find the information. "
+    "If the tool returns an error, inform the user politely. "
+    "If the tool is successful, present the weather report clearly.",
+    tools=[get_weather],  # Pass the function directly
 )

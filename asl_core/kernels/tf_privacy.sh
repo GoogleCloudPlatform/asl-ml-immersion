b396b6b70e840cd2af052c8509e0306cbd50daf1
Takumi Ohyama <takumiohym@google.com>
2026-02-23 03:10:15 +0000

    updated dedicated kernels under asl_core

--- asl_core/kernels/tf_privacy.sh
+++ asl_core/kernels/tf_privacy.sh
@@ -5,31 +5,41 @@
 #
 # This scripts will create a ipython kernel named $ENVNAME
 
+PROJECT_DIR=asl_core/notebooks/responsible_ai
 ENVNAME=tf_privacy_kernel
 KERNEL_DISPLAY_NAME="TF Privacy Kernel"
+PYTHON_VERSION=3.10.14
 
-source ~/.bashrc
+VENV_PYTHON="${ENVNAME}/bin/python"
+VENV_BIN="${ENVNAME}/bin"
+
+cd "$PROJECT_DIR"
 
 # Cleaning up the kernel and exiting if first arg is 'remove'
 if [ "$1" == "remove" ]; then
-  echo Removing kernel $ENVNAME
-  jupyter kernelspec remove $ENVNAME
-  conda env remove -n $ENVNAME -y
-  exit 0
+    echo "Removing kernel spec: $ENVNAME"
+    jupyter kernelspec remove "$ENVNAME" -f || true
+    echo "Removing venv $ENVNAME..."
+    rm -rf "$ENVNAME"
+    exit 0
 fi
 
-conda create -q -n $ENVNAME python=3.10.14 -y
-conda activate $ENVNAME
+# Setup virtual env and kernel
+uv venv "$ENVNAME" --python "$PYTHON_VERSION" --prompt "$KERNEL_DISPLAY_NAME"
+uv pip install -p "$VENV_PYTHON" ipykernel 'setuptools<82' ipywidgets pexpect
+uv pip install -p "$VENV_PYTHON" tensorflow==2.14.0
+uv pip install -p "$VENV_PYTHON" --no-deps tensorflow-privacy==0.8.12 dp_accounting==0.4.3 tensorflow_probability==0.22.0
+uv pip install -p "$VENV_PYTHON" numpy==1.23.5 scipy~=1.9 dm-tree==0.1.8 attrs==25.3.0 cloudpickle==3.1.1 scikit-learn==1.*
 
-# Install packages using a pip local to the conda environment.
-conda install -q pip
-pip install -q ipykernel
+NEW_PATH="${VENV_BIN}:${PATH}"
 
-# Adds the conda kernel.
-DL_ANACONDA_ENV_HOME="${DL_ANACONDA_HOME}/envs/$ENVNAME"
-python -m ipykernel install --prefix "${DL_ANACONDA_ENV_HOME}" --name $ENVNAME --display-name "$KERNEL_DISPLAY_NAME"
-rm -rf "${DL_ANACONDA_ENV_HOME}/share/jupyter/kernels/python3"
+"$VENV_PYTHON" -m ipykernel install \
+    --user \
+    --name="$ENVNAME" \
+    --display-name="$KERNEL_DISPLAY_NAME" \
+    --env PATH "$NEW_PATH"
 
+<<<<<<< HEAD
 # Install packages
 <<<<<<< HEAD:kernels/tf_privacy.sh
 pip install tensorflow==2.14.0
@@ -42,3 +52,6 @@ pip install plotly==6.2.0 nbformat==5.10.4
 >>>>>>> 59e93d3b (added vertex eval package to kernel):kernels/adk.sh
 
 conda deactivate
+=======
+echo "Kernel '$KERNEL_DISPLAY_NAME' is ready."
+>>>>>>> 988bb32c (updated dedicated kernels under asl_core)

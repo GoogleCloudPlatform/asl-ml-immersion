cfcc54bc14021696142126af2775cebeda1cf093
olex-snk <osaienko@google.com>
2026-08-31 21:42:18 +0100

    fixed cloud permission

--- scaffolds/adk_ide/streamlit_adk_app/deploy.sh
+++ scaffolds/adk_ide/streamlit_adk_app/deploy.sh
@@ -46,8 +46,6 @@ gcloud run deploy "$APP_NAME" \
   --cpu 1 \
   --memory 4Gi \
   --region us-central1 \
-  --no-allow-unauthenticated \
-  --invoker-iam-check \
   --update-env-vars GCP_PROJECT="$GOOGLE_CLOUD_PROJECT",GCP_REGION="$GOOGLE_CLOUD_LOCATION",GOOGLE_CLOUD_AGENT_ENGINE_ID="projects/PROJECT_ID/locations/us-central1/reasoningEngines/RUNTIME_ID"
 
 echo 'Deployment Done.'
